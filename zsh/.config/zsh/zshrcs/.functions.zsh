66fc5081cb17504705e752f1cc21d06d2ccb49ee
Shivam <=>
2025-08-28 07:35:03 -0400

    cdr - cd reporoot

--- zsh/.config/zsh/zshrcs/.functions.zsh
+++ zsh/.config/zsh/zshrcs/.functions.zsh
@@ -1,3 +1,14 @@
 function reload() {
     exec zsh
 }
+
+alias cdr='reporoot'
+
+reporoot() {
+    local repo_root
+    repo_root=$(git rev-parse --show-toplevel 2>/dev/null)
+
+    if [[ $? -eq 0 && -n "$repo_root" ]]; then
+        cd "$repo_root" || return 1
+    fi
+}
