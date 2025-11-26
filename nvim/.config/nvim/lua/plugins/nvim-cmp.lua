0146aeb37dd3fa69f6bc27f72d18555f6247b555
Shivam <=>
2025-11-26 15:50:44 -0500

    clean up unused nvim plugins

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- "hrsh7th/nvim-cmp",
-  -- config = function()
-  --   -- configuration to set the color for the paramater in the function signatures
-  --   -- vim.api.nvim_set_hl(0, "CmpItemKindFunctionParameter", { fg = "#98c379" })
-  --   -- vim.
-  -- end,
-}
